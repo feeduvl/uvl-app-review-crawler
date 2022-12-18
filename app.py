0ea73d50228901249e73572845f7f4c293584f92
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-12-18 20:29:35 +0100

    fixed mistake with serve

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from src.flask_setup import app
 if __name__ == '__main__':
     #app.run(debug=False, host="0.0.0.0", port=9661)
     from waitress import serve
-    serve.run(app, host="172.23.0.23", port=9661)
+    serve(app, host="172.23.0.23", port=9661)
 
