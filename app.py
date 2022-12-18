e87bf5b096a07c821931efe497537e98a5bbca1a
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-12-18 11:25:42 +0100

    reverted back to old version

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from src.flask_setup import app
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=9661)
+    app.run(debug=False, host="0.0.0.0", port=9661)
 
