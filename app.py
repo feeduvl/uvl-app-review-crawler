8717077191324abb18532a803e84661569c1cae7
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-12-18 09:35:12 +0100

    add waitress for production server

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from src.flask_setup import app
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=9661)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=9661)
