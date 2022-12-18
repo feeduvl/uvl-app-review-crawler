cbc7cd98bd477522bdd31dd1743032e6d6995680
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-12-18 20:27:03 +0100

    attempt to deploy api with waitress on uni server host

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from src.flask_setup import app
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=9661)
+    #app.run(debug=False, host="0.0.0.0", port=9661)
+    from waitress import serve
+    serve.run(app, host="172.23.0.23", port=9661)
 
