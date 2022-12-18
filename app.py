115864ea4f9819217276bfefd65afc05713af813
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-12-18 11:20:07 +0100

    Revert "add waitress for production server"
    This reverts commit 8717077191324abb18532a803e84661569c1cae7.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from src.flask_setup import app
 
 if __name__ == '__main__':
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=9661)
-    app.run(debug=False, host="0.0.0.0", port=9661)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=9661)
+
