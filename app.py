34fb44a81f30dad90a1e22e7c4a615cfab820a14
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-12-18 20:33:34 +0100

    Revert "reverted back to old version"
    This reverts commit e87bf5b096a07c821931efe497537e98a5bbca1a.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from src.flask_setup import app
 
 if __name__ == '__main__':
-    #app.run(debug=False, host="0.0.0.0", port=9661)
-    from waitress import serve
-    serve(app, host="172.23.0.23", port=9661)
+    app.run(debug=False, host="0.0.0.0", port=9661)
+
 
